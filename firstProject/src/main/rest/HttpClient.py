d4ed9b3afa3360fa5930688dbf7cffedb0707148
Jaime Palomares <jaime.palomares+DFIN@dfinsolutions.com>
2023-05-09 16:48:58 -0500

    Authentication scenarios created

--- firstProject/src/main/rest/HttpClient.py
+++ firstProject/src/main/rest/HttpClient.py
@@ -1,34 +1,36 @@
+import json
+
 import requests
 
 
 class RestMethods:
 
     @staticmethod
-    def get_method(url):
-        response = requests.get(url)
+    def get_method(url, payload, headers):
+        response = requests.get(url, data=json.dumps(payload), headers=headers)
 
         return response
 
     @staticmethod
-    def post_method(url):
-        response = requests.post(url)
+    def post_method(url, payload, headers):
+        response = requests.post(url, data=json.dumps(payload), headers=headers)
 
         return response
 
     @staticmethod
-    def put_method(url):
-        response = requests.put(url)
+    def put_method(url, payload, headers):
+        response = requests.put(url, data=json.dumps(payload), headers=headers)
 
         return response
 
     @staticmethod
-    def patch_method(url):
-        response = requests.patch(url)
+    def patch_method(url, payload, headers):
+        response = requests.patch(url, data=json.dumps(payload), headers=headers)
 
         return response
 
     @staticmethod
-    def delete_method(url):
-        response = requests.delete(url)
+    def delete_method(url, payload, headers):
+        response = requests.delete(url, data=json.dumps(payload), headers=headers)
 
         return response
--- firstProject/src/test/MethodsTest.py
+++ firstProject/src/test/MethodsTest.py
@@ -1,25 +1,36 @@
 import unittest
-from src.main.rest.methods import MethodsRequest
+
+from src.main.rest.request import MethodsRequest
 
 
 class TestMethods(unittest.TestCase):
 
     def test_get_method(self):
-        response = MethodsRequest.GET_method()
+        payload = {"method": "GET", "name": "Jhon", "lastname": "Doe", "age": 33}
+        response = MethodsRequest.GET_method(payload)
+        print(response.json())
         assert response.status_code == 200
 
     def test_post_method(self):
-        response = MethodsRequest.POST_method()
+        payload = {"method": "POST", "name": "Jhon", "lastname": "Doe", "age": 33}
+        response = MethodsRequest.POST_method(payload)
+        print(response.json())
         assert response.status_code == 200
 
     def test_patch_method(self):
-        response = MethodsRequest.PATCH_method()
+        payload = {"method": "PATH", "name": "Jhon", "lastname": "Doe", "age": 33}
+        response = MethodsRequest.PATCH_method(payload)
+        print(response.json())
         assert response.status_code == 200
 
     def test_put_method(self):
-        response = MethodsRequest.PUT_method()
+        payload = {"method": "PUT", "name": "Jhon", "lastname": "Doe", "age": 33}
+        response = MethodsRequest.PUT_method(payload)
+        print(response.json())
         assert response.status_code == 200
 
     def test_delete_method(self):
-        response = MethodsRequest.DELETE_method()
+        payload = {"method": "DELETE", "name": "Jhon", "lastname": "Doe", "age": 33}
+        response = MethodsRequest.DELETE_method(payload)
+        print(response.json())
         assert response.status_code == 200
